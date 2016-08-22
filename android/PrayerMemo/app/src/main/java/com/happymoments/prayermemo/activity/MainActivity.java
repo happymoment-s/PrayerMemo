3ecb0f6c44869ded718d185821a808232c90cdae
Sunghoon Kim <shkim@nemustech.co.kr>
2016-08-22 21:13:16 +0900

    activity 패키지 추가

--- android/PrayerMemo/app/src/main/java/com/happymoments/prayermemo/activity/MainActivity.java
+++ android/PrayerMemo/app/src/main/java/com/happymoments/prayermemo/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.happymoments.prayermemo;
+package com.happymoments.prayermemo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.happymoments.prayermemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
