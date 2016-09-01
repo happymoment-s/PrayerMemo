c7265980a44464cc0952e64bed0430ebcc179405
Sunghoon Kim <shkim@nemustech.co.kr>
2016-09-01 21:30:13 +0900

    header layout 수정(아이콘 추가)

--- android/PrayerMemo/app/src/main/java/com/happymoments/prayermemo/activity/MainActivity.java
+++ android/PrayerMemo/app/src/main/java/com/happymoments/prayermemo/activity/MainActivity.java
@@ -2,24 +2,10 @@ package com.happymoments.prayermemo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.happymoments.prayermemo.R;
-import com.happymoments.prayermemo.data.Memo;
-import com.happymoments.prayermemo.database.DatabaseManagerByRealm;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText mTitleEditText;
-    EditText mContentEditText;
-    Button mAddButton;
-    Button mGetButton;
-    TextView mMemoListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +17,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mTitleEditText = (EditText) findViewById(R.id.editText);
-        mContentEditText = (EditText) findViewById(R.id.editText2);
-        mAddButton = (Button) findViewById(R.id.button);
-        mGetButton = (Button) findViewById(R.id.button2);
-        mMemoListTextView = (TextView) findViewById(R.id.textView);
     }
 
     private void initData() {
-        mAddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Memo memo = new Memo();
-                memo.setTitle(mTitleEditText.getText().toString());
-                memo.setContent(mContentEditText.getText().toString());
-                DatabaseManagerByRealm.addMemo(memo);
-                mTitleEditText.setText("");
-                mContentEditText.setText("");
-            }
-        });
-
-        mGetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                List<Memo> memoList = DatabaseManagerByRealm.getMemoList();
-                mMemoListTextView.setText(memoList.toString());
-            }
-        });
     }
 }
