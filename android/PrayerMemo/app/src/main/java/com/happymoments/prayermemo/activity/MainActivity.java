29323894f3cf14ee68f3281148db2b16af7520fd
Sunghoon Kim <shkim@nemustech.co.kr>
2016-09-12 15:40:44 +0900

    MainActivity에 위젯 초기화 코드 구현

--- android/PrayerMemo/app/src/main/java/com/happymoments/prayermemo/activity/MainActivity.java
+++ android/PrayerMemo/app/src/main/java/com/happymoments/prayermemo/activity/MainActivity.java
@@ -2,10 +2,18 @@ package com.happymoments.prayermemo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.happymoments.prayermemo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mFolderNameTextView;
+    private ImageView mAddMemoImageView;
+    private ImageView mSearchImageView;
+    private ImageView mSettingImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +24,47 @@ public class MainActivity extends AppCompatActivity {
         initData();
     }
 
+    //------- view -------
     private void initView() {
+        mFolderNameTextView = (TextView) findViewById(R.id.header_folder_name_textView);
+        mAddMemoImageView = (ImageView) findViewById(R.id.header_add_memo_imageView);
+        mSearchImageView = (ImageView) findViewById(R.id.header_search_imageView);
+        mSettingImageView = (ImageView) findViewById(R.id.header_setting_imageView);
     }
 
     private void initData() {
+        mFolderNameTextView.setOnClickListener(this);
+        mAddMemoImageView.setOnClickListener(this);
+        mSearchImageView.setOnClickListener(this);
+        mSettingImageView.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.header_folder_name_textView:
+                break;
+            case R.id.header_add_memo_imageView:
+                break;
+            case R.id.header_search_imageView:
+                break;
+            case R.id.header_setting_imageView:
+                break;
+        }
+    }
+    //======= view =======
+
+
+
+    //------- activity cycle --------
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    //======= activity cycle =======
 }
